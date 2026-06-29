32d903a36734d096c2a1f3d3cc7a48308a745584
karrivinay54 <vinaykarri54@gmail.com>
2026-06-29 13:48:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-from rapidfuzz import process
 from streamlit_searchbox import st_searchbox
 
 # ==================================================
